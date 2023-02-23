c8df9411acda236ab0e5594a2bbc412b9c9809e3
AlcalinoGitHub <alcalinouwu69@gmail.com>
2023-02-22 23:54:35 -0600

    Init

---
+++ mysite/MainPage/static/function.js
@@ -0,0 +1,8 @@
+function addText(id){
+    texts = {
+        papeleria: `Una papelería escolar completamente abastecida\nde útiles dentro de la institucion`,
+        
+    }
+
+    console.log(texts[id])
+}

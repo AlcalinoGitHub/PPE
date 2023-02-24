d56fee917e7b31f7e914e928f1cb1f87046e173d
AlcalinoGitHub <alcalinouwu69@gmail.com>
2023-02-24 16:37:30 -0600

    Added propuuesta Model

--- mysite/Content/admin.py
+++ mysite/Content/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Propuesta
+
 # Register your models here.
+
+admin.site.register(Propuesta)
--- mysite/Content/views.py
+++ mysite/Content/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Propuesta
 
 # Create your views here.
 
@@ -10,4 +11,21 @@ def about(request):
     return HttpResponse('About')
 
 def feedback(request):
+    if request.method == 'POST':
+        Title = request.POST.get('Title')
+        Description = request.POST.get('description')
+        Grade = None
+        email = None
+        if request.POST.get('email') is not None: 
+            email = request.POST.get('email')
+        if request.POST.get('grado') is not None:
+            Grade = request.POST.get('grado')
+        NewProposal = Propuesta.objects.create(
+            Title = Title,
+            Description = Description,
+            Grado  = Grade,
+            Email = email
+        )
+        NewProposal.save()
+
     return render(request, 'encuesta.html')

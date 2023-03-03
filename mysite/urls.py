7ded97bfdba99cbfeae40ba279f00676ab4a9169
AlcalinoGitHub <alcalinouwu69@gmail.com>
2023-03-03 16:51:01 -0600

    Added necesary imports to urls.py
    from django.conf import settings
    from django.conf.urls.static import static

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('', include('MainPage.urls')),
     path('', include('Content.urls'))
 ]
-import static 
-import settings
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns += (static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT))
 urlpatterns += (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))

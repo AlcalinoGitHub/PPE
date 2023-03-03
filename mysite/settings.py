68c2e9b872d1edefd938da8948aba36c2ce7038d
AlcalinoGitHub <alcalinouwu69@gmail.com>
2023-03-03 17:31:59 -0600

    Deleted some unncecesary imports

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,5 +134,3 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
-django_heroku.settings(locals())
